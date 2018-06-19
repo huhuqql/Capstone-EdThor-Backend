f6d5942a11ae34533fd2a74f8929cacddff787db
Siyu <schen347@wisc.edu>
2018-06-18 22:43:03 -0400

    fix bug

--- cmu-EdThor-parent/EdThor-web/src/main/java/org/cmu/edthor/capstone/repository/RecordRepository.java
+++ cmu-EdThor-parent/EdThor-web/src/main/java/org/cmu/edthor/capstone/repository/RecordRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface RecordRepository extends MongoRepository<Record, ObjectId> {
 	
-	public Record findByName();
+	public Record findByStudentId(int studentId);
 }
